98299945e759ce4c4e882d9d9c0feb4276890f1e
Sergey Kornienko <skornienko29@gmail.com>
2021-01-13 20:19:29 +0300

    add memoized

---
+++ memoized.py
@@ -0,0 +1,47 @@
+def memoized(func):
+    means = {}
+
+    def inner(arg):
+        if arg in means:
+            return means.get(arg)
+        result = func(arg)
+        means[arg] = result
+        return result
+    return inner
+
+
+if __name__ == '__main__':
+    counter = [0]
+
+    @memoized
+    def xor(byte):
+        counter[0] += 1
+        return 255 ^ byte
+
+
+    assert xor(xor(42)) == 42
+    assert counter == [2], "At this moment xor should be called twice"
+    assert xor(42) + xor(xor(42)) == 255
+    assert counter == [2], "No extra xor calls should be happened"
+    assert xor(127) == 128
+    assert xor(128) == 127
+    assert counter == [4], "Total number of calls should be four"
+
+
+    @memoized
+    def double(x):
+        return x * 2
+
+    @memoized
+    def triple(x):
+        return x * 3
+
+    argument = 42
+    first_result = double(argument)
+    second_result = triple(argument)
+    assert argument * 3 != first_result, (
+        "First function should not provide result for the second one"
+    )
+    assert double(argument) != second_result, (
+        "Second function should not affect memory of first function"
+    )
