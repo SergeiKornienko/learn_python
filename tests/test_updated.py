fc49aa3511581819daa5cc31963a67e93bc56d8d
Sergey Kornienko <skornienko29@gmail.com>
2021-01-23 14:45:46 +0300

    mod updated

---
+++ tests/test_updated.py
@@ -0,0 +1,10 @@
+from functions.updated import updated
+
+
+def test_updated():
+    old = {'a': 1, 'b': None, 2: 4}
+    copy_of_old = old.copy()
+    assert updated(old) is not old
+    assert updated(old, a=2) == {'a': 2, 'b': None, 2: 4}
+    assert old == copy_of_old, "Old dict should stay unchanged!"
+    assert updated({}, foo='bar', bar=42) == {'foo': 'bar', 'bar': 42}
