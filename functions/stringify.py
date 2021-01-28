3ae8bc86c3dcf9a0d87d24d97423040b43bc208f
Sergey Kornienko <skornienko29@gmail.com>
2021-01-28 09:11:10 +0300

    add stringify.py

--- functions/stringify.py
+++ functions/stringify.py
@@ -1,5 +1,23 @@
-def stringify(items, replacer=' ', spaces_count=1):
-    if not isinstance(items, dict):
-        return str(items)
+def stringify(data, replacer=' ', spaces_count=1):
 
-
+    def inner(items, replacer_in=replacer, count=spaces_count):
+        if not isinstance(items, dict):
+            return str(items)
+        list_values = []
+        for key in items.keys():
+            list_values.append(''.join([
+                replacer_in * count,
+                str(key),
+                ': ',
+                str(inner(
+                    items[key],
+                    replacer_in=replacer,
+                    count=count + spaces_count,
+                )),
+            ]))
+        return '\n'.join([
+            '{',
+            *list_values,
+            '{a}{b}'.format(a=(replacer_in * (count - spaces_count)), b='}'),
+        ])
+    return inner(data, replacer_in=replacer, count=spaces_count)
